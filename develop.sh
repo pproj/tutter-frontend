adc4c621e71863482d411d841312c5f4c30eb7c5
marcsello <punkosdmarcell@rocketmail.com>
2024-08-02 21:50:03 +0200

    removed legacy plugin support

--- develop.sh
+++ develop.sh
@@ -2,4 +2,4 @@
 
 # God I hate nodejs so much...
 
-podman run -v $PWD:/app:rw --uidmap 1000:0:1 --uidmap 0:1:1000 --user 1000 -it --workdir /app --network host node:16 /bin/bash
+podman run -v $PWD:/app:rw --uidmap 1000:0:1 --uidmap 0:1:1000 --user 1000 -it --workdir /app --network host node:18 /bin/bash
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
@@ -9,14 +8,10 @@ export default defineConfig({
   assetsDir: "assets",
   plugins: [
     vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
